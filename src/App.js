bc7935873160c309ba36da9b1fffef55428f0345
RashmitShrestha <rashmit_stha@gmail.com>
2020-12-30 13:43:45 -0800

    Rash added the react template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>The weather go brrrrr</h1>
+
+     <img alt="super funnny weather meme" src="https://i.pinimg.com/564x/7c/2b/f8/7c2bf88c0bfb259fded8a1e771cdbe68.jpg" height="auto" width="25%"/>
+    </div>
+  );
+}
+
+export default App;
